308be6ca233f8e19638892237f98a170018eebaf
TataSchultz <tarcila.schultz@gmail.com>
2023-06-14 10:55:52 -0300

    inicio API

---
+++ src/API/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+
+export const Acesso =() =>{
+axios.post(' burger-queen-api-mock-beige.vercel.app/login')
+.then((resposta)=>{
+    console.log('TA AQUI:', resposta.data.accessToken)
+  }).catch((erro)=>{
+      console.log(erro)
+  })
+};
