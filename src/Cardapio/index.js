7f13bda3f4370be979b290669441823b003de09f
daphnevilhar <vilhardaphne@gmail.com>
2023-06-22 19:40:53 -0300

    atualizações no css

--- src/Cardapio/index.js
+++ src/Cardapio/index.js
@@ -13,25 +13,32 @@ export function Cardapio() {
     })
     }, [])
     return(
-        <section>
-        <Switch/>
-        <Botao>
-            Café da manhã
-        </Botao>
-        <Botao>
-            Almoço
-        </Botao><Botao>
-            Bebidas
-        </Botao>
-        <section className='posicao-cards'>
-            {produtos.map((produto) =>
-            <div className='card'>
-            <img src={produto.image} alt=''></img>
-            <p>{produto.name}</p>
-            <p>{produto.price}</p>
-            </div>
-            )}
-        </section>
+        <section className='cardapio'>
+
+            <section className='switch'>
+                <Switch/>
+            </section>
+            
+            <section className='filtros'>
+                <Botao>
+                    Café da manhã
+                </Botao>
+                <Botao>
+                    Almoço
+                </Botao><Botao>
+                    Bebidas
+                </Botao>  
+            </section>
+
+            <section className='posicao-cards'>
+                {produtos.map((produto) =>
+                <div className='card'>
+                <img src={produto.image} alt=''></img>
+                <p>{produto.name}</p>
+                <p>{produto.price}</p>
+                </div>
+                )}
+            </section>
         
         </section>
     )
